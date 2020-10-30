243cdde0586745f46218e13e26f645a572a52b72
tracer-ics2019 <tracer@njuics.org>
2020-10-30 00:49:23 -0700

    >  compile U201714740 yuirito Linux ubuntu 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:49:23 up 2 days, 10:26,  1 user,  load average: 0.07, 0.07, 0.08 31a6f0f96b9d24ba3dea81d2258d5bb15a551fc8

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -47,8 +47,9 @@ static struct rule {
   {"-", TK_SUB, 4},		// substract
   {"\\*", TK_MUL, 5},		// multiply or derefrence
   {"/", TK_DIV, 5},		// divide
-  {"\\$[a-zA-Z]{2,3}", TK_REG,0}, //register
+  
   {"0[Xx][0-9a-fA-F]+", TK_HEX, 0},	// hex (must before the TK_NUM)
+  {"\\$[a-zA-Z]{2,3}", TK_REG,0}, //register
   {"[0-9]+", TK_NUM, 0},	// number(dec)
   {"\\(", TK_LBR, 7},		// left bracket
   {"\\)", TK_RBR, 7},		// right bracket
