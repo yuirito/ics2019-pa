7927caf68a7e976ebc41b6e1fa9fdefa2e833c34
tracer-ics2019 <tracer@njuics.org>
2020-11-19 23:06:35 -0800

    >  compile U201714740 yuirito Linux ubuntu 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:06:35 up 15:20,  1 user,  load average: 0.24, 0.16, 0.10 e1694ae9178986bb2a794bfa3ed08e2eef488b00

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -14,14 +14,35 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:
-    case CC_B:
-    case CC_E:
-    case CC_BE:
-    case CC_S:
-    case CC_L:
-    case CC_LE:
-      TODO();
+    case CC_O:	// OF == 1
+    	rtl_get_OF(&s0);
+    	break;
+    case CC_B:	// CF == 1
+    	rtl_get_CF(&s0);
+    	break;
+    case CC_E:	// ZF == 1
+    	rtl_get_ZF(&s0);
+    	break;
+    case CC_BE:	// CF == 1 or ZF == 1 
+    	rtl_get_CF(&s0);
+    	rtl_get_ZF(&s1);
+    	rtl_or(&s0, &s0, &s1);
+    	break;
+    case CC_S:	// SF == 1 
+    	rtl_get_SF(&s0);
+    	break;
+    case CC_L:	// SF != OF
+    	rtl_get_SF(&s0);
+    	rtl_get_OF(&s1);
+    	rtl_xor(&s0, &s0, &s1);
+    	break;
+    case CC_LE:	// ZF == 1 or SF != OF
+    	rtl_get_SF(&s0);
+    	rtl_get_OF(&s1);
+    	rtl_xor(&s0, &s0, &s1);	// s0 = (SF != OF)
+    	rtl_get_ZF(&s1);		// s1 = (ZF == 1)
+    	rtl_or(&s0, &s0, &s1);
+    	break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
