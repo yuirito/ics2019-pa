8be4f7819eb35b06daf37b079ca167a543d036ea
tracer-ics2019 <tracer@njuics.org>
2020-11-20 05:35:14 -0800

    >  compile U201714740 yuirito Linux ubuntu 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:35:14 up 20:55,  1 user,  load average: 0.13, 0.07, 0.02 eaf3227b3b1074a6ac33bac92208bfa22e33c3d

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -2,7 +2,16 @@
 #include "cc.h"
 
 make_EHelper(test) {
-  TODO();
+  /* PA2.2 */
+  rtl_and(&s0,&id_dest->val,&id_src->val);
+  
+  rtl_update_ZFSF(&s0,id_dest->width);
+ 
+  // CF = 0, OF = 0
+  s1 = 0;
+  rtl_set_CF(&s1);
+  rtl_set_OF(&s1);
+
 
   print_asm_template2(test);
 }
