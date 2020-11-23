391ef7872ffcbc97efd20f89675da3ccfee44161
tracer-ics2019 <tracer@njuics.org>
2020-11-22 19:29:13 -0800

    >  run U201714740 yuirito Linux ubuntu 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:29:13 up  1:59,  1 user,  load average: 0.14, 0.09, 0.06 e14e36d2f9298b38a52266871eda9b81d07b107f

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -36,14 +36,17 @@ char* strcat(char* dst, const char* src) {
 
 int strcmp(const char* s1, const char* s2) {
   assert(s1 != NULL && s2 != NULL);
-  size_t i;
-  for(i=0;s1[1]!='\0'&&s2[i]!='\0';i++){
-    if(s1[i]<s2[i])return -1;
-    else if(s1[i]>s2[i])return 1;
-  }
-  if(s1[i]<s2[i])return -1;
-  else if(s1[i]>s2[i])return 1;
-  return 0;
+  while(*s1 != '\0' && *s1 == *s2 ) {
+		s1 ++;
+		s2 ++;
+	}
+	
+	int ret = *(unsigned char *)s1 - *(unsigned char *)s2;
+	
+	if(ret > 0) ret = 1;
+	else if(ret < 0) ret = -1;
+	
+	return  ret;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
