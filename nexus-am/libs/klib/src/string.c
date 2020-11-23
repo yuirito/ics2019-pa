772ba539bad8b7235eb121b51fda8816bf7810d7
tracer-ics2019 <tracer@njuics.org>
2020-11-22 19:17:54 -0800

    >  run U201714740 yuirito Linux ubuntu 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:17:54 up  1:48,  1 user,  load average: 0.21, 0.11, 0.06 ae0a9d4cacf036128a57892d9d836fb7c8e28151

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -1,41 +1,96 @@
 #include "klib.h"
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
+/* PA2.2 */
 size_t strlen(const char *s) {
-  return 0;
+  if (s == NULL) return 0;
+  size_t len = 0;
+  while ((*s++)!='\0') len++;
+  return len;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  if(dst == NULL || src == NULL) return dst;
+  char *t = dst;
+  while ((*t++ = *src++) !='\0');
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  if(dst == NULL || src == NULL) return dst;
+  char *t = dst;
+  size_t i = 0;
+  for (i=0; i<n&&(*src != '\0'); i++){
+    *t++ = *src++;
+  }
+  return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  if(dst == NULL || src == NULL) return dst;
+  char *t = dst;
+  while (*t++!='\0');
+  while ((*t++ = *src++) !='\0');
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  assert(s1 != NULL && s2 != NULL);
+  while(*s1 != '\0' && *s1==*s2){
+    s1++;
+    s2++;
+  }
+
+  int ret = *s1 - *s2;
+	
+	if(ret > 0) ret = 1;
+	else if(ret < 0) ret = -1;
+	return  ret;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+  assert(s1 != NULL && s2 != NULL);
+	int ret = 0;
+	while(n -- ) {
+		ret = *s1  - *s2;
+		if(ret != 0 || *s1 == '\0') break;
+		s1 ++;
+		s2 ++;
+	}
+	if(ret > 0) ret = 1;
+	else if(ret < 0) ret = -1;
+	return ret;
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  if (v == NULL) return NULL;
+  char *t = (char *)v;
+  while(n -- ) *t ++ = c; 
+  return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+  char *src = (char *)in;
+	char *dst = (char *)out;
+	assert(dst <= src || dst >= src + n );	
+	while(n --) *dst ++ = *src ++;
+	return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  return 0;
+  assert(s1 != NULL && s2 != NULL);
+	unsigned char *v1 = (unsigned char *)s1;
+	unsigned char *v2 = (unsigned char *)s2;
+	int ret = 0;
+	while(n -- ) {
+		ret = *v1  - *v2;
+		if(ret != 0) break;
+		v1 ++;
+		v2 ++;
+	}
+	if(ret > 0) ret = 1;
+	else if(ret < 0) ret = -1;
+	return ret;
 }
 
 #endif
