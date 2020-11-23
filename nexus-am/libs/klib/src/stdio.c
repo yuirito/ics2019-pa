fd07aeda9e07170665d26a6abf61669e598482e8
tracer-ics2019 <tracer@njuics.org>
2020-11-22 19:25:07 -0800

    >  run U201714740 yuirito Linux ubuntu 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:25:07 up  1:55,  1 user,  load average: 0.01, 0.06, 0.05 67c5a51be2e6efde702a7bdf792d291f9f6bfbc8

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -17,6 +17,75 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
+  int length;
+  const char* a;
+  char *str=out;
+  for(;*fmt;++fmt){
+    if(*fmt!='%'){
+      *str++=*fmt;
+      continue;
+    }
+    ++fmt;
+    switch(*fmt){
+      case 's':{
+        a=va_arg(ap,char*);
+        length=strlen(a);
+        for(int i=0;i<length;i++){
+          *str++=*a++;
+        }
+        continue;
+      }
+      case 'd':{
+        int num=va_arg(ap,int);
+        int i=0;
+        char nums[20];
+        if(num==0)nums[0]='0';
+        else{
+          if(num<0){
+            *str++='-';
+            num=(-num);
+          }
+          while(num!=0){
+            nums[i++]=num%10+'0';
+            num/=10;
+          }
+        }
+        for(int j=i-1;j>=0;j--){
+          *str++=nums[j];
+        }
+        break;
+      }
+      case 'c':{
+        int num=va_arg(ap,int);
+        *str++=(char)num;
+        break;
+      }
+      case 'p':
+      case 'x':{
+        uint32_t num = va_arg(ap,uint32_t);
+        int i=0;
+        char nums[20];
+        if(num==0)nums[0]='0';
+        else{
+          while(num!=0){
+            uint32_t x=num%16;
+            if(x<10)nums[i++]=x+'0';
+            else nums[i++]=x-10+'a';
+            num/=16;
+          }
+        }
+        *str++='0';
+        *str++='x';
+        for(int j=i-1;j>=0;j--){
+          *str++=nums[j];
+        }
+        break;
+      }
+    
+      default:break;
+    }
+  }
+  *str='\0';
   return 0;
 }
 
