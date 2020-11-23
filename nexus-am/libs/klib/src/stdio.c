a6cc2fdf9def05191827ce36fd5b1ede2a22b89e
tracer-ics2019 <tracer@njuics.org>
2020-11-22 19:24:36 -0800

    >  run U201714740 yuirito Linux ubuntu 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:24:35 up  1:55,  1 user,  load average: 0.03, 0.07, 0.06 e2ba93b4fb9ad258c6af26bfa81fb66ba8c01b82

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -2,9 +2,18 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
+/* PA2.2 */
 int printf(const char *fmt, ...) {
-  return 0;
+  va_list args;
+  char str[256];
+  int length;
+  va_start(args,fmt);
+  length=vsprintf(str,fmt,args);
+  va_end(args);
+  for(int i=0;str[i]!='\0';i++){
+    _putc(str[i]);
+  }
+  return length;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
@@ -12,7 +21,12 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+  va_list args;
+  int length;
+  va_start(args,fmt);
+  length=vsprintf(out,fmt,args);
+  va_end(args);
+  return length;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
