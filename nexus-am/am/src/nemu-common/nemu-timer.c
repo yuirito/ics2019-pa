671a3aabeb834227d2ed515894a140b53da8ec70
tracer-ics2019 <tracer@njuics.org>
2020-11-29 18:07:56 -0800

    >  run U201714740 yuirito Linux ubuntu 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:07:56 up  5:35,  1 user,  load average: 0.20, 0.08, 0.04 99e439d220109d4dd67d9b66afff228eb2e996f9

--- nexus-am/am/src/nemu-common/nemu-timer.c
+++ nexus-am/am/src/nemu-common/nemu-timer.c
@@ -2,12 +2,16 @@
 #include <amdev.h>
 #include <nemu.h>
 
+/* PA2.3 */
+static uint32_t st_time;
 size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
       _DEV_TIMER_UPTIME_t *uptime = (_DEV_TIMER_UPTIME_t *)buf;
-      uptime->hi = 0;
-      uptime->lo = 0;
+      
+      uint64_t time = inl(RTC_ADDR)-st_time;
+      uptime->hi = (uint32_t)(time>>32);
+      uptime->lo = (uint32_t)(time & 0xffffffff);
       return sizeof(_DEV_TIMER_UPTIME_t);
     }
     case _DEVREG_TIMER_DATE: {
@@ -25,4 +29,5 @@ size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
 }
 
 void __am_timer_init() {
+   st_time=inl(RTC_ADDR);
 }
