4363fc7707da2f3ee18c81272e57d77e679a181c
tracer-ics2019 <tracer@njuics.org>
2020-12-07 23:13:09 -0800

    >  run U201714740 yuirito Linux ubuntu 5.4.0-56-generic #62~18.04.1-Ubuntu SMP Tue Nov 24 10:07:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:13:09 up 1 day, 14:03,  1 user,  load average: 0.77, 0.27, 0.10 c43acd8960b228a4f225c5937056ccd7354c6fef

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,8 +10,24 @@
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  Elf_Ehdr elfheader;
+  Elf_Phdr programheader;
+  size_t offset = 0;
+  size_t p_offset = 0;
+  ramdisk_read(elfheader,0,sizeof(Elf_Ehdr));
+  for (uint16_t i=0; i<elfheader.e_phnum; i++){
+    offset = elfheader.e_phoff;
+    ramdisk_read(programheader,offset,sizeof(Elf_Phdr));
+    offset+=sizeof(Elf_Phdr);
+    if(programheader.p_type == PT_LOAD){
+      uint8_t buf[programheader.p_filesz];
+      ramdisk_read(&buf,programheader.p_offset,programheader.p_filesz);
+      memcpy((void*)programheader.p_vaddr,&buf,programheader.p_filesz);
+      memset((void*)(programheader.p_vaddr+programheader.p_filesz),0,(programheader.p_memsz-programheader.p_filesz));
+    }
+
+  }
+  return elfheader.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
