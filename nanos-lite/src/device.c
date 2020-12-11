0a9b65787d61bcedc48e35724a18a9ef4dc2c8f7
tracer-ics2019 <tracer@njuics.org>
2020-12-11 00:50:50 -0800

    >  run U201714740 yuirito Linux ubuntu 5.4.0-56-generic #62~18.04.1-Ubuntu SMP Tue Nov 24 10:07:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:50:50 up 0 min,  1 user,  load average: 0.34, 0.09, 0.03 1d60e168c86f63645b7fd513b45ccb128a6df523

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -40,7 +40,8 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 static char dispinfo[128] __attribute__((used)) = {};
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  strncpy(buf,dispinfo + offset, len);
+  if(len+offset>strlen(dispinfo))len=strlen(dispinfo)-offset;
+  strncpy(buf, &dispinfo[offset], len);
   return len;
 }
 
